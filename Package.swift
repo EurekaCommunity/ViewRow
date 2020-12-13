d5108a6cae42fc6257468919a0702a91185f363d
Mark Alldritt <alldritt@latenightsw.com>
2020-12-12 16:02:29 -0800

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ViewRow",
+    products: [
+        .library(
+            name: "ViewRow",
+            targets: ["ViewRow"]),
+    ],
+    targets: [
+        .target(
+            name: "ViewRow",
+            dependencies: [],
+            path: "ViewRow"),
+    ]
+)
